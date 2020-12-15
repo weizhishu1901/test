19b0c1c120c4bddeb49d0d4c3ce8364b14f3c579
zhaozuyin <1515503374@qq.com>
2020-12-15 08:20:52 +0800

    Updates

---
+++ precache-manifest.a1ee378dbe0f556149bf740523696ebc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1199bfa8f87cf42f0c6cc65a3f7f0bc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4288c50113bae0493cf2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d85956b26a72425b85c0",
+    "url": "/static/js/2.a2d67585.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2d67585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4288c50113bae0493cf2",
+    "url": "/static/js/main.3a24b3f2.chunk.js"
+  },
+  {
+    "revision": "fd54b029b7d19b595f84",
+    "url": "/static/js/runtime-main.f53b2653.js"
+  }
+]);
